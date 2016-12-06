1d6a983055536eab59b5de88da6438f54eb95913
nikki93 <s.nikhilesh@gmail.com>
2016-12-06 23:22:48 +0000

    Remove `gl.resize(...)` hack again
    fbshipit-source-id: a50726a

--- Scenes/Conformance/GLRunTest.js
+++ Scenes/Conformance/GLRunTest.js
@@ -21,8 +21,6 @@ class GLRunTest extends React.Component {
   _onContextCreate = (gl) => {
     // gl.enableLogging = true;
 
-    gl.resize = (w, h) => console.log('w, h', w, h);
-
     const { testCase, testName, onDone } = this.props;
 
     let testCounter = 0;
@@ -81,7 +79,6 @@ class GLRunTest extends React.Component {
         },
       },
       createContext(width, height, options) {
-        gl.resize(width, height);
         return gl;
       },
     });
