86e6cb152f9dedaa92101a22a72e921e6b926811
Nikhilesh Sigatapu <nikki@Nikhileshs-MacBook-Pro.local>
2016-12-06 12:46:18 +0000

    Start at home screen
    fbshipit-source-id: b233628

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import { Router } from './navigation';
 import { NavigationProvider, StackNavigation } from '@exponent/ex-navigation';
 import Scenes from './Scenes';
 
-// const initialRoute = 'home';
-const initialRoute = Router.getRoute('scene', { component: Scenes.Conformance });
+const initialRoute = 'home';
+// const initialRoute = Router.getRoute('scene', { component: Scenes.Conformance });
 
 Exponent.registerRootComponent(() => (
   <NavigationProvider router={Router}>
